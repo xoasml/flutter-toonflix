228cb0ddedd39e5967476b8fc6fef54af24371fa
TaeHoon <taehoon@TaeHoonui-MacBookAir.local>
2026-01-09 08:54:00 +0900

    api

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    print(webtoons);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 01,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Text(
+          "오늘의 웹툰",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+        ),
+      ),
+      body: FutureBuilder(future: webtoons, builder: (context, snapshot) {
+        if(snapshot.hasData){
+          return Text("T");
+        }else {
+          return Text("F");
+        }
+
+      }),
+    );
+  }
+}
